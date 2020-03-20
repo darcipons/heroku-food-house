fb5dd9cd1effe6180efa6fc0ad903da6fd6f4105
darcipons <darcimarie33@gmail.com>
2020-03-19 20:16:45 -0400

    added react-bootstrap carousel with changing images to about

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import Carousel  from 'react-bootstrap/Carousel'
 import axios from 'axios';
 import './About.css'
 
 const About = () => {
 
     const [photo, setPhoto] = useState({ meals: [] })
+    const [photo2, setPhoto2] = useState({ meals: [] })
+    const [photo3, setPhoto3] = useState({ meals: [] })
 
     const getPhoto = async () => {
       const { data } = await axios(
@@ -13,15 +16,65 @@ const About = () => {
       setPhoto({ meals: data.meals[0] });
     };
 
+    const getPhoto2 = async () => {
+      const { data } = await axios(
+        `https://www.themealdb.com/api/json/v1/1/random.php`
+      );
+      setPhoto2({ meals: data.meals[0] });
+    };
+
+    const getPhoto3 = async () => {
+      const { data } = await axios(
+        `https://www.themealdb.com/api/json/v1/1/random.php`
+      );
+      setPhoto3({ meals: data.meals[0] });
+    };
+
     useEffect(() => {
       getPhoto();
+      getPhoto2();
+      getPhoto3();
     }, [photo[0]]);
-
-    console.log(photo)
 
     return (
       <div>
-        <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={photo.meals.strMealThumb} alt={photo.meals.strMeal}
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={photo2.meals.strMealThumb} alt={photo2.meals.strMeal}
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={photo3.meals.strMealThumb} alt={photo3.meals.strMeal}
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+
+        {/* <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
           <div className="carousel-inner">
             <div class="carousel-item active">
             <img src={photo.meals.strMealThumb} alt={photo.meals.strMeal} className="d-block w-100"></img>
@@ -41,7 +94,7 @@ const About = () => {
             <span className="carousel-control-next-icon" aria-hidden="true"></span>
             <span className="sr-only">Next</span>
           </a>
-        </div>
+        </div> */}
 
         <div className='about'>Food House</div>
         <div className='about-description'>Food House was created during a part-time Front-End Development Course at Wyncode Academy. It uses the mealAPI to access recipes. The User can search for their favorite meals by category. They can also access a random recipe, that changes each time the page is visited or refreshed. Food House was styled using Bootstrap components and CSS.
